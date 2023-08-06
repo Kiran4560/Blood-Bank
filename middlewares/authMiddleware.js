f4e768c21b77df9924d3160c77076c459cb6c7c9
Kiran <kiran2022003kumari@gmail.com>
2023-08-07 01:52:58 +0530

    debug completes

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,12 +3,15 @@ const JWT = require("jsonwebtoken");
 module.exports = async (req, res, next) => {
   try {
     const token = req.headers["authorization"].split(" ")[1];
+    //console.log("tolken",token);
     JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
         return res.status(401).send({
           success: false,
           message: "Auth Failed",
+          
         });
+       
       } else {
         req.body.userId = decode.userId;
         next();
